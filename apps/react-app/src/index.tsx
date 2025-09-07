5efce6bc5249ffc07e5d4a2693be56b6a4fecd6b
Tim Shelburne <shelburt02@gmail.com>
2025-09-07 17:04:34 +0200

    Created template react app + vite

---
+++ apps/react-app/src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return <h1>Hello, React + Vite + TS 👋</h1>;
+}
+
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element not found");
+
+createRoot(container).render(<App />);
